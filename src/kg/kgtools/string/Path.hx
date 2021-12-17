d7d24d3499cdcac357d845e6ca8f93896c34eb25
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-12-17 14:09:20 -0500

    Create Path.hx

---
+++ src/kg/kgtools/string/Path.hx
@@ -0,0 +1,16 @@
+package kg.kgtools.string;
+
+import kglog.Logger;
+
+using StringTools;
+
+class Path {
+  public static function addTrailingBackslash(Path:String):String {
+    Logger.simpleLog("Checking for trailing backslash...");
+    if (Path.endsWith("\\")) { Logger.simpleLog("Trailing backslash found; returning original string"); return Path; } else {
+      var x;
+      x = Path + "\\";
+      return x;
+    }
+  }
+}
